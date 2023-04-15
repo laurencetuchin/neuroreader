03b540cf4699554c92ae728ca4211b2bb0749d33
laurencetuchin <laurencetuchin@gmail.com>
2023-04-15 11:35:43 +1000

    Add in fix for word calculation 40%

--- neuro-reader/src/features/NeuroReader.jsx
+++ neuro-reader/src/features/NeuroReader.jsx
@@ -10,31 +10,37 @@ export default function NeuroReader() {
     }
 
     // function that takes word input length and identifies whether appropriate to bold or not
-    function boldFirstLetters(text, percentage) {
+    // need to update to include min one character if word length < 3 
+    function boldFirstLetters(text) {
         const words = text.split(' ');
         const boldedWords = words.map(word => {
-          const boldLength = Math.floor(word.length * percentage / 100);
-          const boldText = word.slice(0, boldLength);
-          const plainText = word.slice(boldLength);
-          return (
-            <span>
-              <span className="font-bold">
-                {boldText}
+          if (word.length < 3) {
+            return <span className="font-bold">{word}</span>;
+          } else {
+            // percentage calculation of word length
+            const wordLength = word.length * 40 / 100;
+            const boldText = word.slice(0, wordLength);
+            const plainText = word.slice(wordLength);
+            return (
+              <span>
+                <span className="font-bold">
+                  {boldText}
+                </span>
+                {plainText}
               </span>
-              {plainText}
-            </span>
-          );
-        });    
+            );
+          }
+        });
         return boldedWords.reduce((prev, curr) => [prev, ' ', curr]);
-    }
+      }
     
-    return (
+      return (
         <div className="flex flex-col">
           <label className="mb-2 font-bold text-lg" htmlFor="input-text">
             Enter a paragraph:
           </label>
           <div className="flex flex-wrap">
-            {boldFirstLetters(inputValue, 40)}
+            {boldFirstLetters(inputValue)}
           </div>
           <textarea
             className="border border-gray-300 rounded-md py-2 px-4 mt-2"
@@ -45,5 +51,6 @@ export default function NeuroReader() {
           />
         </div>
       );
+    
   
 }
