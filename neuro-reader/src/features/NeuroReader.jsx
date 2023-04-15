16c5298c177e29667e59c7f730e2831c307a8d00
laurencetuchin <laurencetuchin@gmail.com>
2023-04-15 10:53:46 +1000

    Update render 40% of text input

--- neuro-reader/src/features/NeuroReader.jsx
+++ neuro-reader/src/features/NeuroReader.jsx
@@ -9,28 +9,41 @@ export default function NeuroReader() {
         setInputValue(event.target.value);
     }
 
-
+    // function that takes word input length and identifies whether appropriate to bold or not
+    function boldFirstLetters(text, percentage) {
+        const words = text.split(' ');
+        const boldedWords = words.map(word => {
+          const boldLength = Math.floor(word.length * percentage / 100);
+          const boldText = word.slice(0, boldLength);
+          const plainText = word.slice(boldLength);
+          return (
+            <span>
+              <span className="font-bold">
+                {boldText}
+              </span>
+              {plainText}
+            </span>
+          );
+        });    
+        return boldedWords.reduce((prev, curr) => [prev, ' ', curr]);
+    }
     
-  return (
-
-    <div className="flex flex-col">
-      <label className="mb-2 font-bold text-lg" htmlFor="input-text">
-        Enter your text:
-      </label>
-      <div className="flex">
-        <span className="font-bold">
-          {inputValue.slice(0, 3)}
-        </span>
-        {inputValue.slice(3)}
-      </div>
-      <input
-        className="border border-gray-300 rounded-md py-2 px-4 mt-2"
-        type="text"
-        value={inputValue}
-        onChange={handleInputChange}
-        id="input-text"
-      />
-    </div>
-  );
+    return (
+        <div className="flex flex-col">
+          <label className="mb-2 font-bold text-lg" htmlFor="input-text">
+            Enter a paragraph:
+          </label>
+          <div className="flex flex-wrap">
+            {boldFirstLetters(inputValue, 40)}
+          </div>
+          <textarea
+            className="border border-gray-300 rounded-md py-2 px-4 mt-2"
+            value={inputValue}
+            onChange={handleInputChange}
+            id="input-text"
+            rows={5}
+          />
+        </div>
+      );
   
 }
