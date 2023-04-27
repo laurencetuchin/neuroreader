d1cc8a68791a5fba684057650742d0476acffaa8
laurencetuchin <laurencetuchin@gmail.com>
2023-04-27 11:42:17 +1000

    Add font weight option

---
+++ neuro-reader/src/features/AdjustFontWeight.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import { useContext, useState } from 'react';
+import { TextContext, SET_FONT_WEIGHT } from '../utilities/MyContext';
+
+export default function AdjustFontWeight() {
+
+    const [state, dispatch] = useContext(TextContext);
+    const [weight, setWeight] = useState('font-normal');
+
+    const handleFontWeight = (weight) => {
+        dispatch({ type: SET_FONT_WEIGHT, payload: weight})
+        setWeight(weight);
+    }
+
+    const weightOptions = [
+        { weightName: 'thin', weightValue: 'font-thin'},
+        { weightName: 'light', weightValue: 'font-light'},
+        { weightName: 'regular', weightValue: 'font-normal'},
+        { weightName: 'medium', weightValue: 'font-medium'},
+        { weightName: 'black', weightValue: 'font-black'},
+    ]
+
+    const getButtonClass = (weight) => {
+        if (weight.weightValue === weight){
+            return 'border-2 border-black';
+        } 
+        return 'border border-gray-300';
+    }
+  return (
+    <div>
+        <p>Select a weight:</p>
+        {weightOptions.map((weight) =>
+            <button 
+                key={weight.weightValue}
+                className={`border border-gray-300 rounded-md py-2 px-4 mt-2 mr-2 ${weight.fontValue} ${getButtonClass(weight)}`}
+            >
+                {weight.weightName}
+            </button>
+        )}
+    </div>
+  )
+}
