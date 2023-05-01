e094eb250cd1466655c7779bdc26952afee49f5f
laurencetuchin <laurencetuchin@gmail.com>
2023-05-01 14:33:33 +1000

    Clean up information pages

--- neuro-reader/src/utilities/About.jsx
+++ neuro-reader/src/utilities/About.jsx
@@ -11,12 +11,12 @@ export default function About() {
     <>
     
     <div className='container mx-auto'>
-      <div className='py-4'>
+      <div className='py-4 text-left text-slate-500'>
 
     <br />
     
     
-    <p className='text-lg sm:text-left text-cyan-800'>Welcome to Neuro Reader, designed to help neuro diverse learners to read more easily and effectively. Our mission is to provide a platform that allows users to input their text and receive customised reading support to help them achieve their full potential.
+    <p className='xl:text-xl text-lg'>Welcome to Neuro Reader, designed to help neuro diverse learners to read more easily and effectively. Our mission is to provide a platform that allows users to input their text and receive customised reading support to help them achieve their full potential.
     <br />
     <br />
     Our purpose is to make reading accessible to everyone, regardless of their neurodiversity. We understand that reading can be a challenge and frustrating experience for some learners, and we aim to provide a solution that caters to their unique needs. Our website uses innovative technology to identify and address the specific challenges that neuro diverse learners face when reading, whether it be difficulty with decoding, comprehension, or fluency. 
@@ -25,10 +25,10 @@ export default function About() {
     At our core, we are dedicated to helping others. We believe that everyone has the right to access education and information, and we strive to remove the barriers that prevent some learners from doing so. Our team of experts is passionate about making a positive impact on the lives of those who use our platform, and we are committed to continuously improving our services to ensure that our users receive the support they need to succeed.
     </p>
     <br />
+      </div>
     <NavLink to={'/'}>
       <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>Get started &#x27A1;</button>
     </NavLink>
-      </div>
     </div>
     </>
   )
--- neuro-reader/src/utilities/HowToUse.jsx
+++ neuro-reader/src/utilities/HowToUse.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export default function HowToUse() {
 
@@ -9,17 +10,23 @@ export default function HowToUse() {
         {description: 'Start reading the converted text and take advantage of the customised support provided to help you read more easily and effectively.', key: 3}
     ];
   return (
-    <div className='contain mx-auto py-12'>
-        <h1 className='text-2xl mb-12 text-left'>How to use Neuro Reader</h1>
+    <div className='contain mx-auto py-2 text-center'>
+        <h1 className='xl:text-xl text-xl font-bold mb-8 text-slate-700'>How to use Neuro Reader</h1>
         <div className=''>
             
-            <ol className='text-left text-xl space-y-4 '>
+            <ol className='text-left space-y-4 text-slate-500 text-justify font-light'>
                 {howTo.map((instruction, index) => {
                     return (
                         <li key={instruction.key}>{index + 1}. {instruction.description}</li>
                     );
                 })}
             </ol>
+            <div className='m-6'>
+
+            <NavLink to={'/'}>
+      <button className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded'>Get started &#x27A1;</button>
+    </NavLink>  
+            </div>
         </div>
     </div>
   )
